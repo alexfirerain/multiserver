7941ee70b6b916922c790abe1936514fd72c736a
Alexander Lukyanov <lux-nova@mail.ru>
2021-10-11 21:08:34 +0300

    orthodox lambda

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,12 +7,13 @@ public class Main {
           "/spring.svg", "/spring.png", "/resources.html",
           "/styles.css", "/app.js", "/links.html", "/forms.html",
           "/classic.html", "/events.html", "/events.js");
+  public static final int SERVER_PORT = 9999;
 
 
   public static void main(String[] args) {
     System.out.println("MAIN");
-    Server server = new Server(9999, 12, validPaths, "public");
-    server.operate();
+    Server server = new Server(12, validPaths, "public");
+    server.listen(SERVER_PORT);
 
   }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,7 +1,14 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,36 +22,99 @@ public class Server {
                     \r
                     """;
 
-    public final int SERVER_PORT;
     public final List<String> validPaths;
     public final String PUBLIC_DIR;
-
     private final ExecutorService connections;
 
-    public Server(int server_port, int threads, List<String> validPaths, String public_dir) {
-        SERVER_PORT = server_port;
+    public Server(int poolSize, List<String> validPaths, String public_dir) {
         this.validPaths = validPaths;
         PUBLIC_DIR = public_dir;
-        connections = Executors.newFixedThreadPool(threads);
+        connections = Executors.newFixedThreadPool(poolSize);
     }
 
-    public void operate() {
-        System.out.println("OPERATING");
-        try (final var serverSocket = new ServerSocket(SERVER_PORT)) {
+    public void listen(int port) {
+        System.out.println("LISTENING");
+        try (final var serverSocket = new ServerSocket(port)) {
             while (true) {
-                try (final var socket = serverSocket.accept()) {
-                    Connection nextConnect = new Connection(socket, this);
-                    connections.execute(nextConnect);
-                } catch (IOException e) {
-                    System.out.println("CONNECT ERROR");
-                    e.printStackTrace();
-                }
+                    final var socket = serverSocket.accept();
+                    connections.submit(() -> handleConnection(socket));
             }
         } catch (IOException e) {
-            System.out.println("OPERATE_ERROR");
+            System.out.println("LISTEN_ERROR");
             e.printStackTrace();
         }
     }
 
+    private void handleConnection(Socket socket) {
+        System.out.println("HANDLING " + socket.getRemoteSocketAddress());
+            try (   socket;
+                    final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    final var out = new BufferedOutputStream(socket.getOutputStream())
+            ) {
+                final var requestLine = in.readLine();
+                final var parts = requestLine.split(" ");
 
+                if (parts.length != 3) {
+                    // just close socket
+                    return;
+                }
+
+                final var path = parts[1];
+                if (!validPaths.contains(path)) {
+                    out.write((
+                            """
+                                    HTTP/1.1 404 Not Found\r
+                                    Content-Length: 0\r
+                                    Connection: close\r
+                                    \r
+                                    """
+                    ).getBytes());
+                    out.flush();
+                    return;
+                }
+
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            ("""
+                                    HTTP/1.1 200 OK\r
+                                    Content-Type: %s\r
+                                    Content-Length: %d\r
+                                    Connection: close\r
+                                    \r
+                                    """).formatted(mimeType, content.length)
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    return;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        ("""
+                                HTTP/1.1 200 OK\r
+                                Content-Type: %s\r
+                                Content-Length: %d\r
+                                Connection: close\r
+                                \r
+                                """).formatted(mimeType, length)
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            } catch (IOException e) {
+                System.out.println("HANDLE_ERROR");
+                e.printStackTrace();
+            }
+    }
 }
+
+
+
